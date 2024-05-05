0ae0ab4a494088defd2530bdbe690a4d3b96676c
agoudbg <agoudbg@gmail.com>
2024-05-05 14:42:22 +0800

    feat: add emoji detect

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
 
-const token = 'xxxxxxxx:xxxxxxxxxxxxxxxxxx';
+const token = process.env.TOKEN;
+
 const bot = new TelegramBot(token, { polling: true });
 
 const cryingEmojis = [
@@ -21,15 +23,35 @@ const cryingEmojis = [
 ];
 
 bot.on('message', msg => {
+  // stickers
   const { sticker } = msg;
 
-  if (!sticker) {
+  if (sticker) {
+    const { emoji } = sticker;
+
+    if (cryingEmojis.includes(emoji)) {
+      bot.sendMessage(msg.chat.id, '请不要发送带有哭泣表情符号的贴纸。', { reply_to_message_id: msg.message_id });
+    }
+
     return;
   }
 
-  const { emoji } = sticker;
+  // custom emojis
+  const text = msg.text ?? msg.caption;
+  const entities = msg.entities ?? msg.caption_entities;
 
-  if (cryingEmojis.includes(emoji)) {
-    bot.sendMessage(msg.chat.id, '请不要发送带有哭泣表情符号的贴纸。', { reply_to_message_id: msg.message_id });
+  if (text && entities) {
+    const emojis = entities.filter(entity => entity.type === 'custom_emoji');
+
+    // check emoji in text
+    emojis.every(emoji => {
+      const { offset, length } = emoji;
+      const emojiText = text.substring(offset, offset + length);
+
+      if (cryingEmojis.includes(emojiText)) {
+        bot.sendMessage(msg.chat.id, '请不要发送带有哭泣表情符号的自定义表情符号。', { reply_to_message_id: msg.message_id });
+        return false;
+      }
+    });
   }
 });
